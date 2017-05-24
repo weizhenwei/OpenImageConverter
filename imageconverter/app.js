235625dd3e77f2bb2735647c74b94166f800aef9
weizhenwei <weizhenwei1988@gmail.com>
2017-05-24 22:56:07 +0800

    add upload route to repo

--- imageconverter/app.js
+++ imageconverter/app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require("fs");
+var multer  = require('multer');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var test = require('./routes/test');
 
 var app = express();
 
@@ -15,15 +18,45 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(multer({ dest: path.join(__dirname, 'workspace')}).array('image'));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/test', test);
+
+
+// for upload file;
+app.get('/upload', function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "upload.html"));
+});
+app.post('/file_upload', function (req, res) {
+       console.log(req.files[0]);
+
+       console.log("****************************cutting line****************");
+
+       var des_file = path.join(__dirname, "workspace", req.files[0].originalname);
+       console.log("destination file: " + des_file);
+       fs.readFile(req.files[0].path, function (err, data) {
+           fs.writeFile(des_file, data, function (err) {
+               if (err) {
+                   console.log(err);
+               } else {
+                   response = {
+                       message:'File uploaded successfully', filename:req.files[0].originalname };
+               }
+
+               console.log( response );
+               res.end(JSON.stringify(response));
+           });
+       });
+});
+ 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
